0655e823905c7300f9e087d774fbe4a32f5a09d0
hadenlee <hadenlee@vt.edu>
2023-11-02 17:29:46 -0400

    newClaim-2

--- frontend/app/pages/newClaim.tsx
+++ frontend/app/pages/newClaim.tsx
@@ -9,15 +9,16 @@ const newClaim = () => {
     }
   
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <div className="flex flex-col w-full p-8 space-y-4 items-center bg-white sm:w-96 rounded-xl shadow-lg">
           <h1 className="text-2xl font-bold mb-4">Add a New Claim</h1>
           <form onSubmit={handleSubmit}>
-            <input className="w-full px-4 py-2 rounded-lg" type="time" placeholder="Time" required />
-            <input className="w-full px-4 py-2 rounded-lg" type="text" placeholder="Location" required />
-            <input className="w-full px-4 py-2 rounded-lg" type="file" placeholder="Upload photo" required />
-            <textarea className="w-full px-4 py-2 rounded-lg" placeholder="Description" required></textarea>
-            <button className="w-full px-4 py-2 rounded-lg bg-blue-600 text-white" type="submit">Submit</button>
+            <input className="w-full px-4 py-2 rounded-lg mb-4" type="time" placeholder="Time" required />
+            <input className="w-full px-4 py-2 rounded-lg mb-4" type="text" placeholder="Location" required />
+            <p className="text-left w-full mb-2">Add item photo</p>
+            <input className="w-full px-4 py-2 rounded-lg mb-4" type="file" />
+            <textarea className="w-full px-4 py-2 rounded-lg mb-4" placeholder="Description" required></textarea>
+            <button className="w-full px-4 py-2 rounded-lg bg-blue-600 text-white mb-4" type="submit">Submit</button>
           </form>
           {submitted && <p className="mt-4 text-green-500">You have successfully submitted the claim and it is now waiting for the administrator's review.</p>}
         </div>
