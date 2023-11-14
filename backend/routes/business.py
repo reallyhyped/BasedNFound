543b5944ea7630ba80432db548504f194b89380d
Solomon <solomonleang@vt.edu>
2023-11-14 00:32:33 -0500

    Update business route

--- backend/routes/business.py
+++ backend/routes/business.py
@@ -32,3 +32,54 @@ async def read_businesses(database = Depends(get_database)):
         SELECT * FROM business"""
     
     return await database.fetch_all(query)
+
+@router.get("/{username}", response_model=Business)
+async def read_business(username: str, database = Depends(get_database)):
+    find_query = "SELECT * FROM business WHERE username = :username"
+    found_business = await database.fetch_one(find_query, {"username": username})
+
+    if found_business:
+        return found_business
+    else:
+        raise HTTPException(status_code=404, detail="Business not found")
+    
+@router.delete("/{username}")
+async def delete_business(username: str, database = Depends(get_database)):
+    find_query = "SELECT * FROM business WHERE username = :username"
+    found_business = await database.fetch_one(find_query, {"username": username})
+    
+    if found_business:
+        delete_query = "DELETE FROM business WHERE username = :username"
+        await database.execute(delete_query, {"username": username})
+        return {"business deleted"}
+    else:
+        raise HTTPException(status_code=404, detail="Business not found")
+    
+@router.put("/{username}", response_model=Business)
+async def update_business(username: str, update_data: BusinessUpdate, database = Depends(get_database)):
+    find_query = "SELECT * FROM business WHERE username = :username"
+    found_business = await database.fetch_one(find_query, {"username": username})
+
+    if not found_business:
+        raise HTTPException(status_code=404, detail="Business not found")
+
+    update_data_dict = update_data.dict(exclude_unset=True)
+    update_fields = ", ".join([
+        f"{key}=:{key}" for key in update_data_dict.keys()
+    ])
+    if update_fields:
+        update_query = f"UPDATE business SET {update_fields} WHERE username = :username"
+        await database.execute(update_query, {**update_data_dict, "username": username})
+        return await database.fetch_one(find_query, {"username": username})
+    else:
+        raise HTTPException(status_code=400, detail="No fields to update")
+    
+@router.get("/{username}/password")
+async def get_business_password(username: str, database = Depends(get_database)):
+    find_query = "SELECT password FROM business WHERE username = :username"
+    result = await(database.fetch_one(find_query,{"username": username}))
+
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="Business not found")
