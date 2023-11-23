e1b973dcd3ed2ce173f7f3673e33104f8c649b69
kkirpa <kirpa@vt.edu>
2023-11-22 22:47:52 -0500

    added remaining models

---
+++ backend/models/claim_model.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, constr
+from datetime import datetime
+
+class Claim(BaseModel):
+    id: int
+    date: datetime
+    status: str
+    business_id: int
+    user_id: int
+    description: str
+
+class ClaimIn(BaseModel):
+    date: datetime
+    status: str
+    business_id: int
+    user_id: int
+    description: str
+
+class ClaimUpdate(BaseModel):
+    date: datetime
+    status: str
+    business_id: int
+    user_id: int
+    description: str
---
+++ backend/models/item_model.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, constr
+from datetime import datetime
+
+class Item(BaseModel):
+    id: int
+    name: str
+    date: datetime
+    claim_id: int
+    business_id: int
+
+class ItemIn(BaseModel):
+    name: str
+    date: datetime
+    claim_id: int
+    business_id: int
+
+class ItemUpdate(BaseModel):
+    name: str
+    date: datetime
+    claim_id: int
+    business_id: int
