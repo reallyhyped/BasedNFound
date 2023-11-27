cacf58cf1da44999a76436d2893135cefb044f71
kkirpa <kirpa@vt.edu>
2023-11-27 17:22:16 -0500

    route and model for manage

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ from routes.business import router as BusinessRouter
 from routes.item import item_router as ItemRouter
 from routes.admin import admin_router as AdminRouter
 from routes.claim import claim_router as ClaimRouter
+from routes.manage import manage_router as ManageRouter
 
 
 load_dotenv()
@@ -89,5 +90,6 @@ app.include_router(BusinessRouter, tags=["Business"], prefix="/business")
 app.include_router(ItemRouter, tags=["Item"], prefix="/item")
 app.include_router(AdminRouter, tags=["Admin"], prefix="/admin")
 app.include_router(ClaimRouter, tags=["Claim"], prefix='/claim')
+app.include_router(ManageRouter, tags=["Manage"], prefix='/manage')
 
 
