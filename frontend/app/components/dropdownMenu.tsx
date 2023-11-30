019a1f39f7dfa996a8f073c7762d0d28037240c2
John Tomas <jtomas@vt.edu>
2023-11-29 23:51:03 -0500

    show item selected from dropdown

--- frontend/app/components/dropdownMenu.tsx
+++ frontend/app/components/dropdownMenu.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('Filter Category');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -24,14 +25,23 @@ const DropdownMenu = () => {
       });
   }, []);
 
+  const handleCategorySelect = (categoryName) => {
+    setSelectedCategory(categoryName);
+    setIsOpen(false); // Close the dropdown after selection
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="relative inline-block text-left">
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" onClick={() => setIsOpen(!isOpen)}>
-          Show all categories
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {selectedCategory}
         </button>
       </div>
 
@@ -44,6 +54,7 @@ const DropdownMenu = () => {
                 href="#"
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                 role="menuitem"
+                onClick={() => handleCategorySelect(category.name)}
               >
                 {category.name}
               </a>
