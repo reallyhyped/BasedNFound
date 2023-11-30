0459f505581ef5d2b487f90df9de14d00f1a430f
John Tomas <92270251+reallyhyped@users.noreply.github.com>
2023-11-30 14:39:40 -0500

    Revert "Populate categories"

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <footer className="flex flex-col items-center justify-center w-full h-24 border-t p-4 bg-gray-200">
-      <p className="mb-4 text-center">Can't find what you are looking for? You can report a lost item and we'll let you know as soon as we find your item!</p>
-      <Link href="/report_lost">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Report Lost Item</button>
-      </Link>
-    </footer>
-  );
-};
-
-export default Footer;
+    return (
+      <footer className="flex flex-col items-center justify-center w-full h-24 border-t p-4 bg-gray-200">
+        <p className="mb-4 text-center">Can't find what you are looking for? You can file a claim and we'll let you know as soon as we find your item!</p>
+        <Link href="/newClaim">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">File A Claim</button>
+        </Link>
+      </footer>
+    );
+  };
+  
+  export default Footer;
