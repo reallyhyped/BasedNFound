92653ee1ab0fc92ddec8df2fe65251c473464343
John Tomas <jtomas@vt.edu>
2023-11-30 21:51:35 -0500

    add link to admin page

--- frontend/app/account/admin/page.tsx
+++ frontend/app/account/admin/page.tsx
@@ -2,11 +2,12 @@
 import { useSession } from 'next-auth/react';
 import Form from './form';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 export default function RegisterPage() {
     const { data: session } = useSession();
     if (session?.userType != "Administrator") {
         redirect('/');
     }
-    return <Form />;
+    return <div><div><Form /></div><div><Link href="/account/admin/approve_business">Approve Businesses</Link></div></div>
 }
