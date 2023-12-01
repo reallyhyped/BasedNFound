ef7785ffa26f4cce7a6959d24a83e90ce0609db1
reallyhyped <j.johntomas@gmail.com>
2023-12-01 13:35:26 -0500

    move business register to register page

--- frontend/app/account/login/page.tsx
+++ frontend/app/account/login/page.tsx
@@ -1,13 +1,16 @@
-import { getServerSession } from 'next-auth';
-import Form from './form';
-import { redirect } from 'next/navigation';
-import Link from 'next/link';
-
+import { getServerSession } from "next-auth";
+import Form from "./form";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function LoginPage() {
   const session = await getServerSession();
   if (session) {
-    redirect('/');
+    redirect("/");
   }
-  return (<div><Form /><Link href="/account/register">Register</Link><div><Link href="/account/business/register">Are you a business? Create an account here</Link></div></div>);
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
--- frontend/app/account/register/page.tsx
+++ frontend/app/account/register/page.tsx
@@ -1,11 +1,23 @@
-import { getServerSession } from 'next-auth';
-import Form from './form';
-import { redirect } from 'next/navigation';
+import { getServerSession } from "next-auth";
+import Form from "./form";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function RegisterPage() {
   const session = await getServerSession();
   if (session) {
-    redirect('/');
+    redirect("/");
   }
-  return <Form />;
+  return (
+    <div>
+      <div>
+        <Form />
+      </div>
+      <div>
+        <Link href="/account/business/register">
+          Are you a business? Create an account here
+        </Link>
+      </div>
+    </div>
+  );
 }
