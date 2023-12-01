f741197c890e59cc28bba257d6a6af04505d75bd
kkirpa <kirpa@vt.edu>
2023-12-01 16:03:48 -0500

    business will show all lost and found items in the business

--- frontend/app/account/business/page.tsx
+++ frontend/app/account/business/page.tsx
@@ -1,111 +1,60 @@
 import React from 'react';
 import Footer from '../../components/footer';
-import ListItem from '../../components/listitem';
+// import ListItem from '../../components/listitem';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import Card from '../../components/card';
+import { redirect } from 'next/navigation';
+
+interface Item {
+  id: number;
+  name: string;
+  date: string;
+  claim_id: number;
+  business_id: number;
+  description: string;
+  image: string;
+  status: string;
+}
 
 const Business = () => {
-  const foundItems = [
-    {
-      id: 1,
-      name: "Phone",
-      date: "2022-01-01",
-      description: "A black iPhone 12 Pro Max with a black case.",
-      claimed: true,
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 2,
-      name: "Wallet",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A brown leather wallet with a blue stripe.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 3,
-      name: "Keys",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A set of keys with a blue keychain.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 4,
-      name: "Watch",
-      date: "2022-01-01",
-      claimed: false,
-      description: "A silver watch with a black band.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    }]; // Your array of found items
-  const lostItems = [
-    {
-      id: 1,
-      name: "Phone",
-      date: "2022-01-01",
-      claimed: false,
-      description: "A black iPhone 12 Pro Max with a black case.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 2,
-      name: "Wallet",
-      date: "2022-01-01",
-      claimed: false,
-      description: "A brown leather wallet with a blue stripe.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 3,
-      name: "Keys",
-      date: "2022-01-01",
-      claimed: false,
-      description: "A set of keys with a blue keychain.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 4,
-      name: "Watch",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A silver watch with a black band.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 5,
-      name: "Airpods",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A pair of white Airpods.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 6,
-      name: "Glasses",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A pair of black glasses.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-    {
-      id: 7,
-      name: "Laptop",
-      date: "2022-01-01",
-      claimed: true,
-      description: "A black Macbook Pro.",
-      image: "https://images.unsplash.com/photo-1557683316-973673baf926?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbGV0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-    },
-  ]; // Your array of lost items
+
+  const [items, setItems] = useState<Item[][]>([]);
+
+  const { data: session } = useSession();
+  if (session?.userType != "Administrator") {
+    redirect('/');
+  }
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/item');
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const lostItems = items.flat().filter((item) => item.business_id === session.id);
+  const foundItems = items.flat().filter((item) => item.business_id === session.id);
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold mt-4">Business Name</h1>
       <div className="flex flex-wrap justify-center items-start w-5/6">
         <h2 className="w-full text-xl font-bold mt-4">Found Items</h2>
-        {foundItems.map(item => (
-          <ListItem key={item.id} item={item} />
+        {foundItems.map((foundItem, index) => (
+          <Card key={index} item={foundItem} />
         ))}
         <h2 className="w-full text-xl font-bold mt-4">Lost Items</h2>
-        {lostItems.map(item => (
-          <ListItem key={item.id} item={item} />
+        {lostItems.map((lostItem, index) => (
+          <Card key={index} item={lostItem} />
         ))}
       </div>
       <Footer />
