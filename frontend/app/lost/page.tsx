035558b2811523ede6291b83c91518725350866e
Solomon <solomonleang@vt.edu>
2023-12-02 13:43:23 -0500

    Search and category functionality updated

--- frontend/app/lost/page.tsx
+++ frontend/app/lost/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import DropdownMenu from "../components/dropdownMenu"; // Adjust the path as needed
-import Card from "../components/card"; // Adjust the path as needed
-import Pagination from "../components/pagination"; // Adjust the path as needed
-import Footer from "../components/footer"; // Adjust the path as needed
+import DropdownMenu from "../components/dropdownMenu";
+import Card from "../components/card"; 
+import Pagination from "../components/pagination"; 
+import Footer from "../components/footer"; 
 import Link from "next/link";
 
 interface Item {
@@ -22,29 +22,56 @@ const LostPage = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(
     null
   );
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [filteredItems, setFilteredItems] = useState<Item[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const url = selectedCategoryId
+        ? `http://localhost:8000/item/category/${selectedCategoryId}`
+        : "http://localhost:8000/item";
+      const response = await fetch(url);
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = selectedCategoryId
-          ? `http://localhost:8000/item/category/${selectedCategoryId}`
-          : "http://localhost:8000/item";
-        const response = await fetch(url);
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
+    
     fetchData();
   }, [selectedCategoryId]);
 
+  useEffect(() => {
+    // Update filteredItems when items change
+    setFilteredItems(items.filter((item) => item.status === "lost"));
+  }, [items]);
+
   const handleCategorySelect = (categoryId: number) => {
     setSelectedCategoryId(categoryId);
+    setSearchInput(""); // Reset search input on category change
   };
 
+  const handleSearch = () => {
+  if (searchInput.trim() === "") {
+    // If search input is empty, reset filtered items to the original data
+    setFilteredItems(items.filter((item) => item.status === "lost"));
+  } else {
+    // If search input is not empty, filter items based on the search input and "lost" status
+    const newFilteredItems = items.filter(
+      (item) =>
+        item.name.toLowerCase().includes(searchInput.toLowerCase()) &&
+        item.status === "lost"
+    );
+    setFilteredItems(newFilteredItems);
+  }
+};
+
+
   const lostItems = items.filter((item) => item.status === "lost");
+  const lostItemsToShow = filteredItems.length > 0 ? filteredItems : lostItems;
 
   return (
     <div className="flex flex-col items-center">
@@ -60,18 +87,23 @@ const LostPage = () => {
       <div className="flex justify-between items-center w-3/4 p-4 pl-4 pr-4">
         <DropdownMenu onCategorySelect={handleCategorySelect} />
         <div className="flex">
-          <input
+        <input
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
             className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="Search"
-          />
-          <button className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Search
-          </button>
+            />
+          <button
+          onClick={handleSearch}
+          className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Search
+        </button>
         </div>
       </div>
       <div className="flex flex-wrap justify-center items-start w-5/6">
-        {lostItems.map((lostItem) => (
+        {lostItemsToShow.map((lostItem) => (
           <Card key={lostItem.id} item={lostItem} />
         ))}
       </div>
